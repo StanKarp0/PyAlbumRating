1a0ebe6322e4d8c49954110690a7f3d8b07a5fe8
stankarp0 <stankarp0@gmail.com>
2017-05-27 22:07:16 +0200

    Added input forms

--- rating/urls.py
+++ rating/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumDetailView.as_view(), name='album_detail'),
     url(r'^performer/(?P<pk>[0-9]+)/$', views.PerformerDetailView.as_view(), name='performer_detail'),
+    url(r'^performer/add/$', views.performer_add, name='add_performer'),
+    url(r'^album/add/(?P<performer_id>[0-9]+)/$', views.album_add, name='add_album'),
+    url(r'^album/rate/(?P<album_id>[0-9]+)/$', views.album_rate, name='rate_album'),
 ]
--- rating/views.py
+++ rating/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from .models import Album, Rating, Performer
 from django.views import generic
 
@@ -31,3 +31,28 @@ class PerformerDetailView(generic.DetailView):
         context = super(PerformerDetailView, self).get_context_data(**kwargs)
         context['albums'] = kwargs['object'].albums()
         return context
+
+
+def performer_add(request):
+    p = Performer(name=request.POST['name'])
+    p.save()
+    return HttpResponseRedirect(reverse('rating:performer_detail', args=(p.pk,)))
+
+
+def album_add(request, performer_id):
+    Album(
+        performer=get_object_or_404(Performer, pk=performer_id),
+        title=request.POST['title'],
+        pub_year=request.POST['year']
+    ).save()
+    return HttpResponseRedirect(reverse('rating:performer_detail', args=(performer_id,)))
+
+
+def album_rate(request, album_id):
+    Rating(
+        album=get_object_or_404(Album, pk=album_id),
+        rating=request.POST['rating'],
+        style=request.POST['style'],
+        desc=request.POST['desc']
+    ).save()
+    return HttpResponseRedirect(reverse('rating:album_detail', args=(album_id,)))
